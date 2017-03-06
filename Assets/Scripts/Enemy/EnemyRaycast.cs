667f740944912c786aa114e1bd159604aa68a1ed
DBeumans <buncho.gamingnetwork@gmail.com>
2017-03-06 11:39:11 +0100

    added scripts

---
+++ Assets/Scripts/Enemy/EnemyRaycast.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRaycast : MonoBehaviour {
+
+	private EnemyMovement myEnemyMovement;
+
+	[SerializeField] private float myRange;
+
+	private void Start () {
+		myEnemyMovement = GetComponent<EnemyMovement> ();
+	}
+
+	private void Update () {
+		raycast ();
+	}
+
+	private void raycast()
+	{
+		RaycastHit hit;
+		float dist;
+
+		Collider[] objs;
+
+		objs = Physics.OverlapSphere (transform.position + Vector3.up, myRange);
+
+		foreach (Collider c in objs) {
+			if (c.name != this.transform.name) {
+				Debug.Log (c.name);
+			}
+		}
+	}
+}
